f9fd938fb9ea646c8c59709cbf82805771098f3e
sjjeong <sjjeong1225@gmail.com>
2020-08-01 14:02:51 +0900

    프로젝트 생성

---
+++ EatTogether/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  EatTogether
+//
+//  Created by SeokJun Jeong on 2020/08/01.
+//  Copyright © 2020 FourMan. All rights reserved.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+    }
+
+
+
+}
